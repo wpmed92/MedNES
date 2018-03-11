cb9cbf70f2cde3b7608f1935c8a2008bc63726a3
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2018-03-11 21:23:10 +0100

    Initial Commit

---
+++ NES/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NES
+//
+//  Created by Ahmed Harmouche on 2018. 03. 11..
+//  Copyright © 2018. Ahmed Harmouche. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
